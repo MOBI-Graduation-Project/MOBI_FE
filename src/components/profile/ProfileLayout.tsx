2fb0bb8c1615b148a8a7ac9b0622fe754189b37c
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-11 03:00:48 +0900

    feat: 프로필 레이아웃컴포넌트 제작

---
+++ src/components/profile/ProfileLayout.tsx
@@ -0,0 +1,14 @@
+import CloseButton from "@/assets/closeButton.svg";
+
+const ProfileLayout = () => {
+  return (
+    <div>
+      <CloseButton className="fixed top-[19px] right-[22px]" />
+      {/*Background Component영역*/}
+      <div className="bg-yellow-10t h-[439.4px]">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+export default ProfileLayout;
