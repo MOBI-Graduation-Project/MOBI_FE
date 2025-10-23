75164517ac8825638888867089aded2f2e36f68c
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-10-24 00:23:16 +0900

    fix: 채팅방 내부 헤더 상대방닉네임 노출 오류 수정

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -2,22 +2,25 @@ import { useParams, useRouter } from "next/navigation";
 
 import Backbtn from "@/assets/chatbot/backbtn.svg";
 
-import users from "@/mock/user.json";
-
-import { getUserNickname } from "@/utils/chat/getUserNickname";
+import chattingData from "@/mock/chatting.json";
 
 const ChatHeader = () => {
+  const MY_ID = 1; // 내 mock Id값
   const router = useRouter();
   const params = useParams();
-  const senderId = Number(params.roomId);
-  const senderNickname = getUserNickname(senderId, users);
+  const targetRoomId = Number(params.roomId);
+
+  const opponentNickname = chattingData.chats.find(
+    chat => chat.roomId === targetRoomId && chat.senderId !== MY_ID,
+  )?.senderNickname;
+
   return (
     <div className="bg-yellow-10 fixed flex h-[100px] w-full flex-row items-center gap-5 pl-[30px]">
       <button onClick={() => router.back()} className="cursor-pointer">
         <Backbtn />
       </button>
       <div className="text-heading2 text-brown text-stroke-white font-[geekble]">
-        {senderNickname}
+        {opponentNickname}
       </div>
     </div>
   );
