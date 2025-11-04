49b1b4b36f08db2d72b70359027900ce12eb8007
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-04 16:22:49 +0900

    rename: 보유주식 등록 폴더명 수정

---
+++ src/app/investment/holdings/new/page.tsx
@@ -0,0 +1,19 @@
+import BottomBar from "@/components/common/bottomBar";
+import Header from "@/components/common/header";
+import { DropdownMenu } from "@/components/investment/DropdownMenu";
+
+const MyStock = () => {
+  return (
+    <div
+      className="relative min-h-screen w-full overflow-y-auto bg-cover bg-center bg-repeat-y"
+      style={{ backgroundImage: "url('/svgs/bgImage.jpg')" }}
+    >
+      <Header />
+      <div className="fixed top-[200px] left-[30px]">
+        <DropdownMenu />
+      </div>
+      <BottomBar />
+    </div>
+  );
+};
+export default MyStock;
