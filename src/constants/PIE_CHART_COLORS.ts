2e3d2d7c344ac8196ae8131915491eb58d7693b4
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-06 00:02:58 +0900

    feat: 파이차트에서 사용될 컬러차트와 컬러 랜덤 섞는 유틸 추가

---
+++ src/constants/PIE_CHART_COLORS.ts
@@ -0,0 +1,14 @@
+export const PIE_COLORS = [
+  "var(--color-yellow)",
+  "var(--color-yellow-10)",
+  "var(--color-yellow-30)",
+  "var(--color-yellow-40)",
+  "var(--color-yellow-50)",
+  "var(--color-yellow-60)",
+  "var(--color-yellow-70)",
+  "var(--color-yellow-80)",
+  "var(--color-yellow-90)",
+  "var(--color-yellow-100)",
+  "var(--color-yellow-110)",
+  "var(--color-yellow-120)",
+];
