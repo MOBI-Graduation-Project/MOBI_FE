a259fdc961a9e43947683871736a9df3673c144f
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-07 03:04:59 +0900

    refactor: 헤딩타이틀 주식명도 받을 수 있는 형식으로 리팩토링

--- src/app/investment/[stock]/page.tsx
+++ src/app/investment/[stock]/page.tsx
@@ -2,13 +2,32 @@
 
 import { useParams } from "next/navigation";
 
+import HeadingTitle from "@/components/common/HeadingTitle";
+import BottomBar from "@/components/common/bottomBar";
+import Header from "@/components/common/header";
+import DropdownMenu from "@/components/investment/DropdownMenu";
+
 const StockChartPage = () => {
   const params = useParams();
   const stockName = params.stock;
 
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      {stockName} 주식 차트 보기
+    <div
+      className="relative min-h-screen w-full overflow-y-auto bg-cover bg-center bg-repeat-y"
+      style={{ backgroundImage: "url('/svgs/bgImage.jpg')" }}
+    >
+      <Header />
+      <div className="fixed top-[120px] left-[30px]">
+        <DropdownMenu />
+      </div>
+      <div className="flex h-full w-full flex-col items-center justify-center gap-[20px] pt-[100px] pb-[120px]">
+        <HeadingTitle
+          texts={["{stockName}의 주식 예측 차트입니다."]}
+          stockName={stockName as string}
+        />
+      </div>
+
+      <BottomBar />
     </div>
   );
 };
--- src/components/common/HeadingTitle.tsx
+++ src/components/common/HeadingTitle.tsx
@@ -1,10 +1,28 @@
 interface HeadingTitleProps {
   userName?: string;
   texts: string[];
+  stockName?: string;
 }
 
-const HeadingTitle = ({ userName = "사용자", texts }: HeadingTitleProps) => {
-  const replacedTexts = texts.map(text => text.replace("{userName}", userName));
+const HeadingTitle = ({
+  userName = "사용자",
+  texts,
+  stockName,
+}: HeadingTitleProps) => {
+  // TBD 유저이름 zustand로 로그인연동시 가져오기
+
+  const replacedTexts = texts.map(text => {
+    let replaced = text;
+    if (userName) {
+      replaced = replaced.replace("{userName}", userName);
+    } else {
+      replaced = replaced.replace("{userName}", "사용자");
+    }
+    if (stockName) {
+      replaced = replaced.replace("{stockName}", stockName);
+    }
+    return replaced;
+  });
 
   return (
     <div className="flex flex-col gap-1">
