5dd73cc337d15dbfc36e9b7ebe4b03984c57f06e
HeejuChoi01 <chj6875@naver.com>
2025-11-09 06:04:43 +0900

    feat: 사주 결과 페이지에 스크롤 기능 추가

--- src/app/fortuneteller/result/page.tsx
+++ src/app/fortuneteller/result/page.tsx
@@ -30,13 +30,13 @@ const FortuneResult = () => {
 
       <div className="flex flex-1 flex-col items-center justify-center">
         <div className="flex flex-col items-center gap-4">
-          <p className="text-heading1 text-brown text-stroke-white pt-[213px] text-center font-[geekble] leading-tight">
+          <p className="text-heading1 text-brown text-stroke-white pt-[180px] text-center font-[geekble] leading-tight">
             {nickname}님과 {company}의 사주 궁합은?
             <br />
             {company}의 주식 상장일은 {listingDate}입니다.
           </p>
 
-          <div className="mr-[176px] ml-[176px] flex items-center justify-center rounded-[20px] border-[2px] border-black bg-[#FFEEBD] px-30 py-10 shadow-lg">
+          <div className="mr-[176px] ml-[176px] max-h-[360px] w-[calc(100%-352px)] items-center justify-center overflow-y-auto rounded-[20px] border-[2px] border-black bg-[#FFEEBD] px-[30px] py-[20px] shadow-lg">
             <p className="font-pretendard text-brown text-center text-[30px] leading-tight">
               당신과 상대방의 사주를 종합해보면, 금(金)과 토(土)의 기운이 조화를
               이루는 관계로 보입니다. 당신의 강한 금기운이 상대방의 토를
@@ -44,7 +44,17 @@ const FortuneResult = () => {
               사주에 편관(偏官)이 강해 리더십이 뛰어난 반면, 당신의 비견(比肩)과
               식신(食神)이 이를 부드럽게 조절해줄 수 있어요. 다만 상대방의
               목(木) 기운이 다소 과할 수 있으니, 때로는 유연한 태도가 필요할 것
-              같습니다.
+              같습니다. 해주며, 이는 서로에게 안정감을 주는 구조입니다. 특히 상대방의
+              사주에 편관(偏官)이 강해 리더십이 뛰어난 반면, 당신의 비견(比肩)과
+              식신(食神)이 이를 부드럽게 조절해줄 수 있어요. 다만 상대방의
+              목(木) 기운이 다소 과할 수 있으니, 때로는 유연한 태도가 필요할 것
+              같습니다. 보입니다. 당신의 강한 금기운이 상대방의 토를
+              생조해주며, 이는 서로에게 안정감을 주는 구조입니다. 특히 상대방의
+              사주에 편관(偏官)이 강해 리더십이 뛰어난 반면, 당신의 비견(比肩)과
+              식신(食神)이 이를 부드럽게 조절해줄 수 있어요. 다만 상대방의
+              목(木) 기운이 다소 과할 수 있으니, 때로는 유연한 태도가 필요할 것
+              같습니다. 해주며, 이는 서로에게 안정감을 주는 구조입니다. 특히 상대방의
+              사주에 편관(偏官)이 강해 리더십
             </p>
           </div>
 
