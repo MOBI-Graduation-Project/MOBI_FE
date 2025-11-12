4d04930a75c05bafb69325f838015c3b7cd0165c
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-12 21:36:43 +0900

    design: 로그인 로드 화면 ui 수정

--- src/components/auth/GoogleCallback.tsx
+++ src/components/auth/GoogleCallback.tsx
@@ -6,6 +6,8 @@ import { useEffect } from "react";
 
 import { handleOAuthCallback } from "@/utils/signup/handleAuthCallback";
 
+import HeadingTitle from "../common/HeadingTitle";
+
 export default function GoogleCallback() {
   const searchParams = useSearchParams();
   const code = searchParams.get("code");
@@ -15,5 +17,12 @@ export default function GoogleCallback() {
     handleOAuthCallback(code);
   }, [code]);
 
-  return <div>로그인 처리 중...</div>;
+  return (
+    <div
+      className="flex h-screen w-full flex-col items-center justify-center gap-[91.27px] bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: "url('/svgs/bgImage.jpg')" }}
+    >
+      <HeadingTitle texts={["로그인 처리 중..."]} />
+    </div>
+  );
 }
