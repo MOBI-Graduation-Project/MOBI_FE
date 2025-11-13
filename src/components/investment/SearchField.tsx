3937b34cb605a9f2c093e8e54d01497c28e15192
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-14 04:47:15 +0900

    refactor: 외부에서 입력받아 prop으로 내려오는 방식으로 제작

--- src/components/investment/SearchField.tsx
+++ src/components/investment/SearchField.tsx
@@ -1,20 +1,21 @@
 "use client";
 
-import { useState } from "react";
-
 import SearchIcon from "@/assets/chatting/searchIcon.svg";
 
-export const SearchField = () => {
-  const [input, setInput] = useState("");
+interface SearchFieldProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
+export const SearchField = ({ value, onChange }: SearchFieldProps) => {
   return (
     <div className="bg-yellow-light flex items-center gap-2 rounded-[30px] px-4 py-2">
       <input
         className="text-brown text-lab2 flex-1 bg-transparent font-[pretendard] placeholder-gray-500 outline-none"
         type="text"
         placeholder="주식명을 입력하세요."
-        value={input}
-        onChange={e => setInput(e.target.value)}
+        value={value}
+        onChange={onChange}
       />
       <SearchIcon className="text-brown-700 h-[24px] w-[24px] cursor-pointer transition-transform hover:scale-110" />
     </div>
