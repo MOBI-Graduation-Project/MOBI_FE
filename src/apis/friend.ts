c0b891c563cf61796235a589f7d1ffec83f7a111
HeejuChoi01 <chj6875@naver.com>
2025-11-15 18:05:44 +0900

    feat: 친구 닉네임 검색 API 함수 추가

---
+++ src/apis/friend.ts
@@ -0,0 +1,18 @@
+import { apiClient } from "./apiClient";
+
+export const searchUserByNickname = async (nickname: string) => {
+  const accessToken = localStorage.getItem("accessToken");
+  if (!accessToken) throw new Error("로그인 필요");
+
+  try {
+    const res = await apiClient.get("/members/search", {
+      params: { nickname },
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+
+    return res.data; // { isSuccess, code, message, result: SearchUser[] }
+  } catch (error) {
+    console.error("searchUserByNickname Error:", error);
+    throw error;
+  }
+};
