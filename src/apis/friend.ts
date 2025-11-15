ed9ce0ee8e7ae10129118ac3795a3add8241b4ae
HeejuChoi01 <chj6875@naver.com>
2025-11-15 23:21:21 +0900

    feat: 친구리스트 API 함수 추가

--- src/apis/friend.ts
+++ src/apis/friend.ts
@@ -16,3 +16,18 @@ export const searchUserByNickname = async (nickname: string) => {
     throw error;
   }
 };
+
+export const getFriends = async () => {
+  const accessToken = localStorage.getItem("accessToken");
+  if (!accessToken) throw new Error("로그인 필요");
+
+  try {
+    const res = await apiClient.get("/friends", {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+    return res.data; // { isSuccess, code, message, result: { friendList, friendRequestList } }
+  } catch (error) {
+    console.error("getFriends Error:", error);
+    throw error;
+  }
+};
