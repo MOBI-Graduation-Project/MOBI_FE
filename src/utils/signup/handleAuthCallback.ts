fc83c92c07b239c50e82982ae426c3a57a74b946
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-17 18:58:46 +0900

    fix: 로그인 후 zustand에만 상태 저장방식으로 수정

--- src/utils/signup/handleAuthCallback.ts
+++ src/utils/signup/handleAuthCallback.ts
@@ -1,5 +1,8 @@
-import { getMyProfile } from "@/apis/profile";
+import { useRouter } from "next/navigation";
+
 import { useUserStore } from "@/stores/userStore";
+
+import { getMyProfile } from "@/apis/profile";
 
 interface OAuthResponse {
   isSuccess: boolean;
@@ -19,7 +22,10 @@ interface OAuthResponse {
   };
 }
 
-export const handleOAuthCallback = async (code: string) => {
+export const handleOAuthCallback = async (
+  code: string,
+  router: ReturnType<typeof useRouter>,
+) => {
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_BASE_URL}/auth/google`,
@@ -56,6 +62,9 @@ export const handleOAuthCallback = async (code: string) => {
           nickname,
           avatarCode: avatar ?? null,
         });
+        const currentState = useUserStore.getState();
+        console.log(currentState.memberId);
+        console.log(currentState.nickname);
       } else {
         console.error("내 프로필 조회 실패:", profileRes?.message);
       }
@@ -64,9 +73,9 @@ export const handleOAuthCallback = async (code: string) => {
     }
 
     if (data.result.isNewMember) {
-      window.location.href = "/signup/nickname";
+      router.push("/signup/nickname");
     } else {
-      window.location.href = "/map";
+      router.push("/map");
     }
   } catch (err) {
     console.error("OAuth 처리 중 에러:", err);
