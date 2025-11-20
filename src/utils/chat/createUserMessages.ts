fa9fa219daacbd9d8d7b657dba0f10ed7b884d65
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-20 16:19:21 +0900

    refactor: 하드코딩된 사용자 정보 수정

--- src/utils/chat/createUserMessages.ts
+++ src/utils/chat/createUserMessages.ts
@@ -1,9 +1,14 @@
+import { useUserStore } from "@/stores/userStore";
+
 import { Message } from "@/types/chatMessage";
+
+const senderId = useUserStore.getState().memberId || undefined;
+const senderNickname = useUserStore.getState().nickname || "사용자";
 
 export const createUserMessage = (text: string, roomId: number): Message => ({
   roomId,
-  senderId: 1,
-  senderNickname: "사용자",
+  senderId: senderId,
+  senderNickname: senderNickname,
   content: text,
   sentAt: new Date().toISOString(),
   read: true,
