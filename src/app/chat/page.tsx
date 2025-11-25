4b599afd03810230f5fcfc415e3dc66ff9978701
신수진 <shinsujin@sinsujin-ui-MacBookAir.local>
2025-11-25 18:55:06 +0900

    refactor: Next/Image형식으로 img태그 수정

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 import { useEffect, useState } from "react";
@@ -52,10 +53,12 @@ const ChatList = () => {
               className="flex items-center justify-between rounded-2xl bg-white/90 px-[20px] py-5 shadow-md transition-all hover:bg-yellow-50"
             >
               <div className="flex flex-row items-center gap-4">
-                <img
+                <Image
                   src={room.otherMemberProfileImage}
                   alt={room.roomName}
-                  className="h-14 w-14 rounded-full object-cover"
+                  width={40}
+                  height={40}
+                  className="rounded-full object-cover"
                 />
                 <div className="text-lab2 text-brown font-[geekble]">
                   {room.roomName}
